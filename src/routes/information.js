33cd39ea2135609e009da1219c50a097964f72cf
Beto Najera <beto_najera@comunidad.unam.mx>
2021-12-29 10:09:30 -0600

    :zap: perf: Remove information/exchange enpoint
    Remove infomration/exchange enpoint

--- src/routes/information.js
+++ src/routes/information.js
@@ -1,6 +1,5 @@
 // Dependencies
 import express from 'express'
-import axios from 'axios'
 
 // Other ddependencies
 import { tokenValidationHandler } from '../utils/middlewares/validationHandler'
@@ -11,7 +10,7 @@ import response from '../utils/network/response'
 const scraping = new Scraping()
 const router = new express.Router()
 
-router.get('/news', async (req, res, next) => {
+router.get('/news', tokenValidationHandler(token), async (req, res, next) => {
 	try {
 		await scraping.news()
 		response.success(res, `Si se pudo`, { secces: true }, 201)
@@ -19,27 +18,5 @@ router.get('/news', async (req, res, next) => {
 		next(err)
 	}
 })
-
-router.get(
-	'/exchange',
-	tokenValidationHandler(token),
-	async (req, res, next) => {
-		try {
-			const { data } = await axios.get(
-				'https://www.deperu.com/api/rest/cotizaciondolar.json'
-			)
-			data.nombre = 'SUNAT'
-
-			response.success(
-				res,
-				`Data find`,
-				{ ...data.Cotizacion[0], nombre: 'SUNAT', actualizado: new Date() },
-				200
-			)
-		} catch (err) {
-			next(err)
-		}
-	}
-)
 
 export default router
